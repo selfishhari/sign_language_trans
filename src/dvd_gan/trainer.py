34d7a9a3f12757a51d69ee76ff3693a4b82f9e54
Narahari B M <narahari.bm@protonmail.com>
2020-04-08 19:19:35 +0530

    fixing save videos

--- src/dvd_gan/trainer.py
+++ src/dvd_gan/trainer.py
@@ -358,9 +358,9 @@ class Trainer(object):
                 for i in range(self.n_class):
                     for j in range(self.test_batch_size):
                         if self.use_tensorboard is True:
-                            self.writer.add_image("Class_%d_No.%d/Step_%d" % (i, j, step), make_grid(denorm(fake_videos[i * self.test_batch_size + j].data)), step)
+                            self.writer.add_image("Class_%d_No.%d/Step_%d" % (i, j, step), make_grid(denorm(fake_videos[i].data)), step)
                         else:
-                            save_image(denorm(fake_videos[0].data), os.path.join(self.sample_path, "generated_image_{}".format(str(step))))
+                            save_image(denorm(fake_videos[i].data), os.path.join(self.sample_path, "generated_image_{}".format(str(step))))
                             break
                 # print('Saved sample images {}_fake.png'.format(step))
                 self.G.train()
